368c428a799a3101e78b44c120d082be3ea0fad9
Rahul Varma <rahulvarma300@gmail.com>
2024-02-10 01:43:29 +0530

    Featch event

--- src/Component/EventHandling/featchEvent.jsx
+++ src/Component/EventHandling/featchEvent.jsx
@@ -11,7 +11,7 @@ this approch will invock the handleClick function  */}
         <button onClick={handleClick}>Click</button>
 {/* event handling by passing inline arrow function. this approch invocks an anonimus arrowfunction which turn call handle Click with evant and 'btn1' argument. this allow ypu to pass aditional data along with the event */}
         <button onClick={evt => handleClick(evt,'btn1')}>Click1</button>
-        <button onClick={}>Click2</button>
+        <button onClick={handleClick.bind(this,'btn2')}>Click2</button>
     </div>
   )
 }
